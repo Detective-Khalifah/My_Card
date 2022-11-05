74c987eda9c8048a74dd998c35956a2bbdf984e2
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2022-11-05 20:45:29 +0100

    Wrap {@link ImageButton}s with {@link MaterialCardView}s

--- app/src/main/java/com/blogspot/thengnet/mycard/FullscreenActivity.java
+++ app/src/main/java/com/blogspot/thengnet/mycard/FullscreenActivity.java
@@ -69,7 +69,7 @@ public class FullscreenActivity extends AppCompatActivity {
             if (actionBar != null) {
                 actionBar.show();
             }
-//            mControlsView.setVisibility(View.VISIBLE);
+            mControlsView.setVisibility(View.VISIBLE);
         }
     };
     private boolean mVisible;
@@ -153,7 +153,7 @@ public class FullscreenActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.hide();
         }
-//        mControlsView.setVisibility(View.GONE);
+        mControlsView.setVisibility(View.GONE);
         mVisible = false;
 
         // Schedule a runnable to remove the status and navigation bar after a delay
