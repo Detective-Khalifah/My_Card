69f9dbd34f81d054d7420552525705f3825b810e
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2022-11-06 02:34:28 +0100

    Configure Kotlin in Project

--- app/src/main/java/com/blogspot/thengnet/mycard/FullscreenActivity.kt
+++ app/src/main/java/com/blogspot/thengnet/mycard/FullscreenActivity.kt
@@ -1,60 +1,35 @@
-package com.blogspot.thengnet.mycard;
+package com.blogspot.thengnet.mycard
 
-import android.annotation.SuppressLint;
-
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import android.os.Build;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.WindowInsets;
-
-import com.blogspot.thengnet.mycard.databinding.ActivityFullscreenBinding;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.AppBarConfiguration.Builder
+import androidx.navigation.ui.NavigationUI.navigateUp
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import com.blogspot.thengnet.mycard.databinding.ActivityFullscreenBinding
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class FullscreenActivity extends AppCompatActivity {
-
-    private ActivityFullscreenBinding binding;
-    private AppBarConfiguration appBarConfiguration;
-
-    @Override
-    protected void onCreate (Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        binding = ActivityFullscreenBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+class FullscreenActivity : AppCompatActivity() {
+    private var binding: ActivityFullscreenBinding? = null
+    private var appBarConfiguration: AppBarConfiguration? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityFullscreenBinding.inflate(
+            layoutInflater
+        )
+        setContentView(binding!!.root)
+        val navController = findNavController(this, R.id.nav_host_fragment_content_main)
+        appBarConfiguration = Builder(navController.graph).build()
+        setupActionBarWithNavController(this, navController, appBarConfiguration!!)
     }
 
-    @Override
-    protected void onPostCreate (Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-//        delayedHide(100);
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(this, R.id.nav_host_fragment_content_main)
+        return (navigateUp(navController, appBarConfiguration!!)
+                || super.onSupportNavigateUp())
     }
-
-    @Override
-    public boolean onSupportNavigateUp () {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
-
 }
